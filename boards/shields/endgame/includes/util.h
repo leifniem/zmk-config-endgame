de2bc83f74c6f7ca276272db51b33dc9ecf80241
Leif Niemczik <leif.niemczik@seibert.group>
2025-12-29 15:36:51 +0100

    reset combos

--- boards/shields/endgame/includes/util.h
+++
@@ -1,6 +0,0 @@
-#define COMBO(name, keypress, keypos) \
-combo_##name {                        \
-   timeout-ms = <100>;                \
-   bindings = <keypress>;             \
-   key-positions = <keypos>;          \
-};
